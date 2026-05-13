396c19168cd0e3626f90ba7fed068bf01eec37db
lvan <258706614@qq.com>
2026-05-13 10:55:01 +0800

    Remove unwanted files (target/, .idea/, *.iml, maven-wrapper.jar)

--- gateways/src/test/java/com/dp/gateways/GatewaysApplicationTests.java
+++ gateways/src/test/java/com/dp/gateways/GatewaysApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.dp.gateways;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class GatewaysApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.dp.gateways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
